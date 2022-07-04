b9bebf4ecaab5dfd2568cf6465ac2633bde01492
nicko <ikar2021@hotmail.com>
2022-07-04 19:27:29 +0300

    Add registration action

--- selfstorage/management/commands/bot.py
+++ selfstorage/management/commands/bot.py
@@ -3,64 +3,110 @@ from environs import Env
 import telegram
 from telegram.ext import (ConversationHandler, CommandHandler, Filters,
                           MessageHandler, Updater)
+from .db_processing import (create_customer, create_order, get_customer,
+                            get_warehouse, get_box,
+                            get_free_boxes_from_warehouse)
+from django.core.exceptions import ObjectDoesNotExist
 
 (
     CREATE_ORDER,
     MY_ORDERS,
-    NAME,
+    FIRST_NAME,
+    LAST_NAME,
+    PATRONIC,
     PHONE,
     FINISH_REGISTRATION,
     USER_CHOICE,
-    CHECK_CHOICE
-    ) = range(7)
+    CHECK_CHOICE,
+    NAME
+    ) = range(10)
 
 
 def start(update, context):
     custom_keyboard = [
-        ['Создать заказ'],
-        ['Зарегистрироваться'],
+        ['Создать заказ'], #
         ['Мои заказы'],
         ]
     reply_markup = telegram.ReplyKeyboardMarkup(
         custom_keyboard
         )
-    start_message = 'Здравствуйте! Я бот компании Складмэн!\n'\
+    start_message_new_user = 'Здравствуйте! '\
+        'Я бот компании Сервис SelfStorage!\n'\
         'У нас вы можете разместить '\
         'и хранить свои вещи в целости и сохранности.\n'\
-        'Если вы еще не пользовались нашими услугами и хотели бы заказать '\
-        'хранение, нажмите «Зарегистрироваться».'
-    update.message.reply_text(start_message, reply_markup=reply_markup)
+        'Похоже, что вы еще не пользовались нашими услугами и хотели бы  '\
+        'заказать хранение. Для этого вам необходимо зарегистрироваться.\n'\
+        'Вы согласны на обработку персональных данных, '\
+        'для регистрирации в нашем сервисе?'
+    start_message_exist_user = 'Здравствуйте! '\
+        'Я бот компании Сервис SelfStorage!\n'\
+        'У нас вы можете разместить '\
+        'и хранить свои вещи в целости и сохранности.\n'\
+        'Чем могу вам помочь ?  '
+    nickname = update.message.chat.username
+    try:
+        get_customer(nickname)
+        update.message.reply_text(
+            start_message_exist_user,
+            reply_markup=reply_markup,
+            )
+    except ObjectDoesNotExist:
+        update.message.reply_text(start_message_new_user)
     return CHECK_CHOICE
-
-# TODO Согласие на обработку перс. данных
 
 
 def check_user_input(update, context):
     user_input = update.message.text
-    if "Зарегистрироваться" in user_input:
+    if 'Да'.lower() == user_input:
         update.message.reply_text(
             'Укажите свой email',
             reply_markup=telegram.ReplyKeyboardRemove(),
             )
         return PHONE
+    else:
+        update.message.reply_text(
+            'Для регистрации в сервисе нужно принять согласие '
+            'на обработку персональных данных. \nДо новых встреч!',
+            reply_markup=telegram.ReplyKeyboardRemove(),
+            )
+        return ConversationHandler.END
 
 
 def phone(update, context):
     update.message.reply_text('Укажите свой телефон в формате +7')
     context.user_data["email"] = update.message.text
-    return NAME
+    return FIRST_NAME
 
 
-def name(update, context):
-    update.message.reply_text('Укажите свою фамилию и имя')
+def first_name(update, context):
+    update.message.reply_text('Укажите свое имя')
     context.user_data["phone"] = update.message.text
+    return LAST_NAME
+
+
+def last_name(update, context):
+    update.message.reply_text('Укажите свою фамилию')
+    context.user_data["first_name"] = update.message.text
+    return PATRONIC
+
+
+def patronic(update, context):
+    update.message.reply_text('Укажите свое отчество')
+    context.user_data["last_name"] = update.message.text
     return FINISH_REGISTRATION
 
 
 def finish_registration(update, context):
-    context.user_data["name"] = update.message.text
+    context.user_data["patronic"] = update.message.text
     update.message.reply_text('Регистрация успешно завершена!')
     update.message.reply_text('Ваши данные:{}'.format(context.user_data))
+    nickname = update.message.chat.username
+    create_customer(context.user_data["first_name"],
+                    context.user_data["last_name"],
+                    nickname,
+                    context.user_data["phone"],
+                    patronymic=context.user_data["patronic"],
+                    e_mail=context.user_data["email"])
     return ConversationHandler.END
 
 
@@ -105,7 +151,9 @@ class Command(BaseCommand):
                     ],
                 CHECK_CHOICE: [MessageHandler(Filters.text, check_user_input)],
                 PHONE: [MessageHandler(Filters.text, phone)],
-                NAME: [MessageHandler(Filters.text, name)],
+                FIRST_NAME: [MessageHandler(Filters.text, first_name)],
+                LAST_NAME: [MessageHandler(Filters.text, last_name)],
+                PATRONIC: [MessageHandler(Filters.text, patronic)],
                 FINISH_REGISTRATION: [MessageHandler(
                     Filters.text,
                     finish_registration,
